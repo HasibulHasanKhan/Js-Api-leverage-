dd0e34b8b0287cb5fdd6235dc28d343dbfcd7c93
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2024-11-11 03:35:27 +0600

    update post and get method

--- vanilaJsApi/crud/get.js
+++ vanilaJsApi/crud/get.js
@@ -1,18 +1,47 @@
-async function getData(id) {
+// async function getData(id) {
+//   try {
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts/${id}`
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(`Error: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     console.log("Post fetched:", data);
+//     console.log("Post ID:", data.id);
+//   } catch (crror) {
+//     console.log("Error:", crror);
+//   }
+// }
+
+async function getPostWithComments(postId) {
   try {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${id}`
+    // Fetch post
+    const postResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
     );
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status}`);
+    if (!postResponse.ok) {
+      throw new Error(`Post fetch error: ${postResponse.status}`);
     }
+    const post = await postResponse.json();
 
-    const data = await response.json();
-    console.log("Post fetched:", data);
-    console.log("Post ID:", data.id);
-  } catch (crror) {
-    console.log("Error:", crror);
+    // Fetch comments related to this post
+    const commentsResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
+    );
+    if (!commentsResponse.ok) {
+      throw new Error(`Comments fetch error: ${commentsResponse.status}`);
+    }
+    const comments = await commentsResponse.json();
+
+    // Combine post and comments
+    const postWithComments = { ...post, comments };
+    console.log("Post with comments:", postWithComments);
+  } catch (error) {
+    console.error("Error fetching post with comments:", error);
   }
 }
-getData(1);
+
+getPostWithComments(1);
--- vanilaJsApi/crud/post.js
+++ vanilaJsApi/crud/post.js
@@ -1,26 +1,56 @@
-async function createPost() {
+// async function createPost() {
+//   const postData = {
+//     title: "Hasibul Hasan Khan",
+//     body: "This is the body part of new post",
+//     section: "B",
+//     userId: 1,
+//   };
+//   try {
+//     const respose = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(postData),
+//     });
+
+//     if (!respose.ok) {
+//       throw new Error("Error: ${respose.status}");
+//     }
+//     const data = await respose.json();
+//     console.log("Post created:", data);
+//   } catch (error) {
+//     console.log("Error creating post:", error);
+//   }
+// }
+// createPost();
+async function postCommentData() {
   const postData = {
-    title: "Hasibul Hasan Khan",
-    body: "This is the body part of new post",
-    section: "B",
+    title: "Complex Post",
+    body: "This is a post with embedded comments",
     userId: 1,
+    comments: [
+      { id: 1, context: "Great post!" },
+      { id: 2, context: "Thanks for sharing" },
+    ],
   };
+
   try {
     const respose = await fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Context-Type": "application/json",
       },
       body: JSON.stringify(postData),
     });
 
     if (!respose.ok) {
-      throw new Error("Error: ${respose.status}");
+      throw new Error(`Error: ${respose.status}`);
     }
     const data = await respose.json();
-    console.log("Post created:", data);
+    console.log("Post created with comments:", data);
   } catch (error) {
-    console.log("Error creating post:", error);
+    console.log("Error:", error);
   }
 }
-createPost();
+postCommentData();
